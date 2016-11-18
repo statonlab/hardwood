417fae6af7d133e67a399c1b718fdec17637c5d2
Abdullah Almsaeed <almasaeed2010@gmail.com>
2016-11-18 15:04:14 -0500

    Update hardwood.js

--- dist/js/hardwood.js
+++ dist/js/hardwood.js
@@ -1,9 +1,8 @@
 $(function() {
 
     $("#elasticsearch_hits_table tr td:last-of-type").each(function() {
-        var text = $(this).text();
-
-        console.log(text);
+        var text = $(this).html();
+        console.log(text.split("<br>"));
     });
 
 });
