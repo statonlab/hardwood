71bd8aa06393a2cd199e1c5aeea38cead3b56ac3
Abdullah Almsaeed <almasaeed2010@gmail.com>
2016-11-18 15:15:15 -0500

    Update hardwood.js

--- dist/js/hardwood.js
+++ dist/js/hardwood.js
@@ -2,7 +2,31 @@ $(function() {
 
     $("#elasticsearch_hits_table tr td:last-of-type").each(function() {
         var text = $(this).html();
-        console.log(text.split("<br>"));
+        var array = text.split("<br>");
+
+        if(array.length > 2) {
+            var div = $("<div />", {});
+            div.append(array.shift() + '<br>' + array.shift());
+            var hidden = $('<div />', {'class':'hidden-hit'}).css('display', 'none');
+            hidden.append(array.join('<br>'));
+            div.append(hidden);
+            var btn = $("<button />", {
+                'type': 'button',
+                'class': 'btn btn-secondary btn-sm'
+            }).click(function(e) {
+                e.preventDefault();
+
+                var hidden_hit = $(this).closest('.hidden-hit');
+                if(hidden_hit.hasClass('is_open')) {
+                    hidden_hit.removeClass('is_open');
+                    hidden_hit.slideUp();
+                } else {
+                    hidden_hit.slideDown();
+                    hidden_hit.addClass('is_open');
+                }
+            });
+            div.append(btn);
+        }
     });
 
 });
