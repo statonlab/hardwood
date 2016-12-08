77bbeffc0c0a54c2c6942df1875480bd10110b7b
Abdullah Almsaeed <almasaeed2010@gmail.com>
2016-12-08 10:57:27 -0500

    Update search box positioning

--- templates/system/page--front.tpl.php
+++ templates/system/page--front.tpl.php
@@ -73,144 +73,149 @@
 ?>
 
 <div id="page-wrapper">
-  <div id="page">
+ <div id="page">
 
-    <?php if ($main_menu || $secondary_menu): ?>
-      <nav id="navigation" class="main-header navbar navbar-light bg-faded navbar-sticky-top">
-        <div class="container">
-          <a class="navbar-brand" href="/">
-            <span class="logo-icon"><i class="fa fa-leaf"></i></span>
-            <div class="logo-wrapper">
-              <span class="logo-text"><?php print variable_get('site_name') ?></span>
-              <div class="text-muted text-xs">
-                <?php print variable_get('site_slogan') ?>
-              </div>
-            </div>
-          </a>
-          <button class="navbar-toggler hidden-lg-up float-xs-right" type="button" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">&nbsp;</button>
-          <div class="clearfix hidden-lg-up"></div>
-          <div class="collapse navbar-toggleable-md" id="navbar-collapse">
-            <?php
-            if (!empty($primary_nav)):
-              print render($primary_nav);
-            endif;
-            ?>
-          </div>
+   <?php if ($main_menu || $secondary_menu): ?>
+    <nav id="navigation" class="main-header navbar navbar-light bg-faded navbar-sticky-top">
+     <div class="container">
+      <a class="navbar-brand" href="/">
+       <span class="logo-icon"><i class="fa fa-leaf"></i></span>
+       <div class="logo-wrapper">
+        <span class="logo-text"><?php print variable_get('site_name') ?></span>
+        <div class="text-muted text-xs">
+          <?php print variable_get('site_slogan') ?>
         </div>
-      </nav> <!-- /.section, /#navigation -->
-    <?php endif; ?>
-
-    <!-- FRONT PAGE JUMBOTRON -->
-    <div class="jumbotron front-page">
-      <div class="jumbotron-inner">
-        <div class="container">
-          <h1 class="text-xs-center">
-            Welcome to the Hardwood Genomics Project
-          </h1>
-          <p class="lead text-xs-center">
-            Genomic resources for hardwood trees
-          </p>
-          <div class="margin-top-1 text-xs-center">
-            <a href="/contact" class="btn btn-success">Contribute Data</a>
-            <a href="/contact" class="btn btn-outline-secondary">Contact Us</a>
-          </div>
-        </div>
+       </div>
+      </a>
+      <button class="navbar-toggler hidden-lg-up float-xs-right" type="button" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+       &nbsp;</button>
+      <div class="clearfix hidden-lg-up"></div>
+      <div class="collapse navbar-toggleable-md" id="navbar-collapse">
+        <?php
+        if (!empty($primary_nav)):
+          print render($primary_nav);
+        endif;
+        ?>
       </div>
+     </div>
+    </nav> <!-- /.section, /#navigation -->
+   <?php endif; ?>
+
+  <!-- FRONT PAGE JUMBOTRON -->
+  <div class="jumbotron front-page">
+   <div class="jumbotron-inner">
+    <div class="container">
+     <div class="row">
+      <div class="col-xs-12">
+       <h1 class="text-xs-center">
+        Welcome to the Hardwood Genomics Project
+       </h1>
+       <p class="lead text-xs-center">
+        Genomic resources for hardwood trees
+       </p>
+       <div class="margin-top-1 text-xs-center">
+        <a href="/contact" class="btn btn-success">Contribute Data</a>
+        <a href="/contact" class="btn btn-outline-secondary">Contact Us</a>
+       </div>
+      </div><!-- /.col -->
+     </div><!-- /.row -->
+    </div><!-- /.container -->
+   </div><!-- /.jumbotron-inner -->
+  </div><!-- /.jumbotron -->
+
+   <?php if ($page['front_search']): ?>
+    <div class="section bg-gray">
+     <div class="container">
+      <div class="row">
+       <div class="col-xs-12 col-lg-10 offset-lg-1 col-xl-6 offset-xl-3 input-group-lg">
+         <?php print render($page['front_search']); ?>
+       </div>
+      </div>
+     </div>
     </div>
+   <?php endif; ?>
 
-    <?php if ($page['front_search']): ?>
-      <div class="section bg-gray">
-        <div class="container">
-          <div class="row">
-            <div class="col-xs-12 col-lg-10 offset-lg-1 col-xl-6 offset-xl-3 input-group-lg">
-              <?php print render($page['front_search']); ?>
-            </div>
-          </div>
-        </div>
+   <?php if ($messages): ?>
+    <div class="section">
+     <div class="container">
+      <div class="row">
+       <div class="col-xs-12">
+         <?php print $messages; ?>
+       </div>
       </div>
-    <?php endif; ?>
+     </div>
+    </div>
+   <?php endif; ?>
 
-    <?php if ($messages): ?>
+  <div id="main-wrapper">
+   <div id="main" class="clearfix">
+
+     <?php $main_content_classes = !empty($page['sidebar_first']) || !empty($page['sidebar_second']) ? 'col-sm-8 col-md-9' : 'col-xs-12'; ?>
+     <?php if ($page['content']): ?>
       <div class="section">
-        <div class="container">
-          <div class="row">
-            <div class="col-xs-12">
-              <?php print $messages; ?>
-            </div>
-          </div>
-        </div>
-      </div>
-    <?php endif; ?>
-
-    <div id="main-wrapper">
-      <div id="main" class="clearfix">
-
-        <?php $main_content_classes = !empty($page['sidebar_first']) || !empty($page['sidebar_second']) ? 'col-sm-8 col-md-9' : 'col-xs-12'; ?>
-        <?php if ($page['content']): ?>
-          <div class="section">
-            <div class="container">
-              <div class="row">
-                <div class="<?php print $main_content_classes; ?>">
-                  <a id="main-content"></a>
-                  <?php if ($tabs): ?>
-                    <?php print render($tabs); ?>
-                  <?php endif; ?>
-
-                  <?php print render($page['content']); ?>
-
-                </div>
-
-                <div class="col-sm-4 col-md-3">
-                  <?php if ($page['sidebar_first']): ?>
-                    <div class="card">
-                      <div class="card-block">
-                        <div id="sidebar-first" class="column sidebar">
-                          <?php print render($page['sidebar_first']); ?>
-                        </div> <!-- /#sidebar-first -->
-                      </div>
-                    </div>
-
-                  <?php endif; ?>
-
-                  <?php if ($page['sidebar_second']): ?>
-                    <div class="card">
-                      <div class="card-block">
-                        <div id="sidebar-second" class="column sidebar">
-                          <?php print render($page['sidebar_second']); ?>
-                        </div> <!-- /#sidebar-second -->
-                      </div>
-                    </div>
-
-                  <?php endif; ?>
-                </div>
-
-              </div>
-            </div>
-          </div>
-        <?php endif; ?>
-      </div>
-    </div> <!-- /#main, /#main-wrapper -->
-
-    <div id="footer" class="secondary-footer">
-      <div class="container">
+       <div class="container">
         <div class="row">
-          <div class="col-xs-12">
-            <?php print render($page['footer']); ?>
-          </div>
-        </div>
-      </div>
-    </div> <!-- /#footer -->
+         <div class="<?php print $main_content_classes; ?>">
+          <a id="main-content"></a>
+           <?php if ($tabs): ?>
+             <?php print render($tabs); ?>
+           <?php endif; ?>
 
-    <div class="section bg-inverse">
-      <div class="container">
-        <div class="col-sm-4">
-          <img src="<?php print base_path() . path_to_theme() . '/dist/images/ut3.png'; ?>" alt="UTK Logo" class="img-fluid img-max-h100">
+           <?php print render($page['content']); ?>
+
+         </div>
+
+         <div class="col-sm-4 col-md-3">
+           <?php if ($page['sidebar_first']): ?>
+            <div class="card">
+             <div class="card-block">
+              <div id="sidebar-first" class="column sidebar">
+                <?php print render($page['sidebar_first']); ?>
+              </div> <!-- /#sidebar-first -->
+             </div>
+            </div>
+
+           <?php endif; ?>
+
+           <?php if ($page['sidebar_second']): ?>
+            <div class="card">
+             <div class="card-block">
+              <div id="sidebar-second" class="column sidebar">
+                <?php print render($page['sidebar_second']); ?>
+              </div> <!-- /#sidebar-second -->
+             </div>
+            </div>
+
+           <?php endif; ?>
+         </div>
+
         </div>
-        <div class="col-sm-4 text-xs-center">
-          <img src="<?php print base_path() . path_to_theme() . '/dist/images/nsf.gif'; ?>" alt="NSF Logo" class="img-fluid img-max-h100">
-        </div>
+       </div>
       </div>
+     <?php endif; ?>
+   </div>
+  </div> <!-- /#main, /#main-wrapper -->
+
+  <div id="footer" class="secondary-footer">
+   <div class="container">
+    <div class="row">
+     <div class="col-xs-12">
+       <?php print render($page['footer']); ?>
+     </div>
     </div>
+   </div>
+  </div> <!-- /#footer -->
 
+  <div class="section bg-inverse">
+   <div class="container">
+    <div class="col-sm-4">
+     <img src="<?php print base_path() . path_to_theme() . '/dist/images/ut3.png'; ?>" alt="UTK Logo" class="img-fluid img-max-h100">
+    </div>
+    <div class="col-sm-4 text-xs-center">
+     <img src="<?php print base_path() . path_to_theme() . '/dist/images/nsf.gif'; ?>" alt="NSF Logo" class="img-fluid img-max-h100">
+    </div>
+   </div>
   </div>
+
+ </div>
 </div> <!-- /#page, /#page-wrapper -->
