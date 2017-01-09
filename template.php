28211c8b810a14a3a83ec2f03d06866827a7fe8a
Abdullah Almsaeed <almasaeed2010@gmail.com>
2017-01-09 08:17:47 -0500

    Update progress bar markup

--- template.php
+++ template.php
@@ -379,3 +379,25 @@ function hardwood_js_alter(&$javascript) {
   unset($javascript['misc/collapse.js']);
   drupal_add_js(drupal_get_path('theme', 'hardwood') . '/dist/js/collapse.js', array());
 }
+
+/**
+ * Implements theme_progress_bar()
+ *
+ * Changes the progress bar markup to match BS4
+ *
+ * @param $variables
+ * @return string
+ */
+function theme_progress_bar($variables) {
+  $output = "<div class=\"progress\">";
+  $output .= "<div class=\"progress-bar\" role=\"progressbar\" aria-valuenow=\"{$variables['percent']}\" aria-valuemin=\"0\" aria-valuemax=\"100\"></div>";
+  $output .= "</div>";
+
+  $output .= '<div id="progress" class="progress">';
+  $output .= '<div class="bar"><div class="filled" style="width: ' . $variables['percent'] . '%"></div></div>';
+  $output .= '<div class="percentage">' . $variables['percent'] . '%</div>';
+  $output .= '<div class="message">' . $variables['message'] . '</div>';
+  $output .= '</div>';
+
+  return $output;
+}
