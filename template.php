f14cce57c23f8b948368b4652902384ba4c98e0c
Abdullah Almsaeed <almasaeed2010@gmail.com>
2017-01-09 08:28:48 -0500

    Update progress bar markup

--- template.php
+++ template.php
@@ -390,14 +390,9 @@ function hardwood_js_alter(&$javascript) {
  */
 function hardwood_progress_bar($variables) {
   $output = "<div class=\"progress\">";
-  $output .= "<div class=\"progress-bar\" role=\"progressbar\" aria-valuenow=\"{$variables['percent']}\" aria-valuemin=\"0\" aria-valuemax=\"100\"></div>";
+  $output .= "<div class=\"progress-bar\" role=\"progressbar\" style=\"width:{$variables['percent']}%\" aria-valuenow=\"{$variables['percent']}\" aria-valuemin=\"0\" aria-valuemax=\"100\">{$variables['percent']}%</div>";
   $output .= "</div>";
-
-  $output .= '<div id="progress" class="progress">';
-  $output .= '<div class="bar"><div class="filled" style="width: ' . $variables['percent'] . '%"></div></div>';
-  $output .= '<div class="percentage">' . $variables['percent'] . '%</div>';
   $output .= '<div class="message">' . $variables['message'] . '</div>';
-  $output .= '</div>';
 
   return $output;
 }
