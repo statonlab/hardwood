6d2cab53b61eb7d8341ffcd32d92ea69a310ef9e
Abdullah Almsaeed <almasaeed2010@gmail.com>
2017-01-12 21:59:01 -0800

    Update dropdown menu in navbar

--- templates/system/navbar.inc.php
+++ templates/system/navbar.inc.php
@@ -1,27 +1,27 @@
 <?php if ($main_menu || $secondary_menu): ?>
-  <nav id="navigation" class="main-header navbar navbar-light bg-faded navbar-full navbar-toggleable-md">
-    <div class="container">
-      <a class="navbar-brand" href="/">
-        <span class="logo-icon"><i class="fa fa-leaf"></i></span>
-        <div class="logo-wrapper">
-          <span class="logo-text"><?php print variable_get('site_name') ?></span>
-          <div class="text-muted text-xs hidden-xs-down">
-            <?php print variable_get('site_slogan') ?>
-          </div>
-        </div>
-      </a>
+ <nav id="navigation" class="main-header navbar navbar-light bg-faded navbar-full navbar-toggleable-md">
+  <div class="container">
+   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+   </button>
 
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbar-collapse">
-        <?php
-        if (!empty($primary_nav)):
-          print render($primary_nav);
-        endif;
-        ?>
-      </div>
+   <a class="navbar-brand" href="/">
+    <span class="logo-icon"><i class="fa fa-leaf"></i></span>
+    <div class="logo-wrapper">
+     <span class="logo-text"><?php print variable_get('site_name') ?></span>
+     <div class="text-muted text-xs hidden-xs-down">
+       <?php print variable_get('site_slogan') ?>
+     </div>
     </div>
-  </nav> <!-- /.section, /#navigation -->
+   </a>
+
+   <div class="collapse navbar-collapse" id="navbar-collapse">
+     <?php
+     if (!empty($primary_nav)):
+       print render($primary_nav);
+     endif;
+     ?>
+   </div>
+  </div>
+ </nav> <!-- /.section, /#navigation -->
 <?php endif; ?>
