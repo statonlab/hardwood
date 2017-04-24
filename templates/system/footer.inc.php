b2a98edcbe6ac7de6e30379e4f36cd95de6bb392
Abdullah Almsaeed <almasaeed2010@gmail.com>
2017-04-24 09:10:15 -0400

    Add tripal logo

--- templates/system/footer.inc.php
+++ templates/system/footer.inc.php
@@ -1,12 +1,21 @@
 <div class="section bg-inverse">
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-4">
-        <img src="<?php print base_path() . path_to_theme() . '/dist/images/ut3.png'; ?>" alt="UTK Logo" class="img-fluid img-max-h100">
-      </div><!-- /.col -->
-      <div class="col-sm-4 text-center">
-        <img src="<?php print base_path() . path_to_theme() . '/dist/images/nsf.gif'; ?>" alt="NSF Logo" class="img-fluid img-max-h100">
-      </div><!-- /.col -->
-    </div><!-- /.row -->
-  </div><!-- /.container -->
+ <div class="container">
+  <div class="row">
+   <div class="col-sm-4">
+    <a href="https://ag.tennessee.edu/Pages/default.aspx" target="_blank">
+     <img src="<?php print base_path() . path_to_theme() . '/dist/images/ut3.png'; ?>" alt="UTK Logo" class="img-fluid img-max-h100">
+    </a>
+   </div><!-- /.col -->
+   <div class="col-sm-4 text-center">
+    <a href="https://www.nsf.gov" target="_blank">
+     <img src="<?php print base_path() . path_to_theme() . '/dist/images/nsf.gif'; ?>" alt="NSF Logo" class="img-fluid img-max-h100">
+    </a>
+   </div><!-- /.col -->
+   <div class="col-sm-4 text-center">
+    <a href="http://tripal.info" target="_blank">
+     <img src="<?php print base_path() . path_to_theme() . '/dist/images/powered_by_tripal.png'; ?>" alt="Tripal Logo" class="img-fluid img-max-h100">
+    </a>
+   </div><!-- /.col -->
+  </div><!-- /.row -->
+ </div><!-- /.container -->
 </div><!-- /.section -->
