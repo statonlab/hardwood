ef44a70cba94832dd34c8c56bc82d00b79048a94
Abdullah Almsaeed <almasaeed2010@gmail.com>
2017-06-27 10:49:20 -0400

    Updates

--- template.php
+++ template.php
@@ -491,7 +491,7 @@ function hardwood_form_node_form_alter(&$form, &$form_state, $form_id) {
   }
 
   dpm($nid);
-  $dpm($cards);
+  dpm($cards);
 
   $form['#submit'][] = 'hardwood_from_node_submit';
 }
