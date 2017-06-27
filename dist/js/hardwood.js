4f8cd76cea6c5e5b10a935d148b2d43dae93200e
Abdullah Almsaeed <almasaeed2010@gmail.com>
2017-06-27 10:16:37 -0400

    Updates

--- dist/js/hardwood.js
+++ dist/js/hardwood.js
@@ -44,7 +44,7 @@
         $('#block-tripal-elasticsearch-website-search-category a').each(function () {
             var text = $(this).text().split(' ');
             var num = text.pop();
-            text = text.join(' ').replace('_', ' ');
+            text = text.join(' ').split('_').join(' ');
             $(this).html(text);
             $(this).append($('<span />', {'class': 'float-right'}).text(num));
         });
