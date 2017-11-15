d560e96db6fe1059fe85ea7e3c484e0d8dd59701
Abdullah Almsaeed <almasaeed2010@gmail.com>
2017-11-15 12:30:27 -0500

    test

--- dist/js/hardwood.js
+++ dist/js/hardwood.js
@@ -11,8 +11,8 @@
 
         $('.tripal_pane-toc-list-item-link').each(function () {
             var id = '.tripal_pane-fieldset-' + $(this).attr('id');
-            console.log(id, $(id));
             if (!$(id)) {
+                console.log($(this))
                 $(this).parents('.views-row').first().remove();
             }
         });
