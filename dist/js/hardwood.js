aa1d37b6629361aa5cf00aa831d3661f0e9cb367
Abdullah Almsaeed <almasaeed2010@gmail.com>
2018-09-28 08:25:22 -0400

    Fix number of days until feedback modal shows up again

--- dist/js/hardwood.js
+++ dist/js/hardwood.js
@@ -371,8 +371,8 @@
       });
 
       var item    = ds.getItem('hardwoods.survey');
-      // 30 days
-      var seconds = 1000 * 60 * 24 * 30;
+      // 3 days
+      var seconds = 1000 * 60 * 60 * 24 * 3;
       if (item) {
         item = parseInt(item);
 
